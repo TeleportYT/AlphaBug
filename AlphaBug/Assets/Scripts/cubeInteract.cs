147af6d5fffc4712e57c8d5c5afe03bbfd51a62d
TeleportYT <vivien110705@gmail.com>
2021-11-12 17:55:36 +0200

    Fixed cube pickup
    fixed cube pickup script (not its works),
    still needs to clean it and make it more clean.

--- AlphaBug/Assets/Scripts/cubeInteract.cs
+++ AlphaBug/Assets/Scripts/cubeInteract.cs
@@ -5,27 +5,30 @@ using UnityEngine;
 public class cubeInteract : MonoBehaviour
 {
     public GameObject plCam;
-    private Vector3 pos = new Vector3(0, -0.39f, 1.45f);
+    private Vector3 pos = new Vector3(0, 0, 1.45f);
     public Rigidbody rb;
     public bool isHolding;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
     }
     public void EnableRagdoll()
     {
+        transform.SetParent(null);
+        //Vector3 newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        //transform.position = newPos;
         gameObject.AddComponent<Rigidbody>();
-        transform.SetParent(null, false);
-        Vector3 newPos = new Vector3(transform.position.x + 10, transform.position.y, transform.position.z);
         Rigidbody rb = gameObject.GetComponent<Rigidbody>();
-
+        rb.AddForce(transform.forward*300f);
 
     }
     public void DisableRagdoll()
     {
+        rb = GetComponent<Rigidbody>();
         Destroy(rb);
         transform.SetParent(plCam.transform, false);
+        Quaternion qr = new Quaternion(0, 0, 0, 0);
         transform.localPosition = pos;
+        transform.localRotation = qr;
     }
 
     private void Update()
